58bb4402879aa2b0dcf4af839f51128773029a24
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-09-03 19:09:45 +0200

    pytacz master

--- Obroty/XII-2014/update1/KROK_0_SAMAA_PLN_TO_NULL.sql
+++
@@ -1,4 +0,0 @@
-BEGIN 
-UPDATE T_EQU_SAPF set "SAAMA_PLN"=NULL
-  WHERE  "SAVFR" >= (SELECT To_Date('01-12-2014','dd-mm-yyyy') FROM dual) AND "SAVFR" <= (SELECT To_Date('31-12-2014','dd-mm-yyyy') FROM dual);
-END;
